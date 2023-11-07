21814f3b366226bb9c726b795bdad328771d5e54
Dang Thai Ha <dth6323@gmail.com>
2023-11-07 10:32:44 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Btllttq7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgv = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgv
+            // 
+            this.dtgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgv.Location = new System.Drawing.Point(425, 27);
+            this.dtgv.Name = "dtgv";
+            this.dtgv.RowHeadersWidth = 51;
+            this.dtgv.RowTemplate.Height = 24;
+            this.dtgv.Size = new System.Drawing.Size(643, 483);
+            this.dtgv.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1080, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 522);
+            this.Controls.Add(this.dtgv);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgv;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+    }
+}
+
---
+++ ProcessDb.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Btllttq7
+{
+    internal class ProcessDb
+    {
+        SqlConnection conn;
+        public void KetNoi()
+        {
+            conn = new SqlConnection("Data Source=THAIHA\\TESTDB;Initial Catalog=lttq;User ID=sa;Password=123456");
+
+            if (conn.State != ConnectionState.Open)
+            {
+                conn.Open();
+
+            }
+        }
+        public void DongKet()
+        {
+            conn = new SqlConnection("Data Source=THAIHA\\TESTDB;Initial Catalog=lttq;User ID=sa;Password=123456");
+
+            if (conn.State != ConnectionState.Closed)
+                conn.Close();
+            conn.Dispose();
+        }
+        public DataTable DocBangsql(string sql)
+        {
+            KetNoi();
+            DataTable tb = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(sql, conn);
+            da.Fill(tb);
+            DongKet();
+            return tb;
+
+        }
+        public void CapNhat(String sql)
+        {
+            SqlCommand cmd = new SqlCommand();
+            KetNoi();
+            cmd.CommandText = sql;
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+            DongKet();
+            cmd.Dispose();
+        }
+    }
+}
